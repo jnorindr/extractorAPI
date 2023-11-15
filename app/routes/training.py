6b95aa0c67544161a08ed0951b546cdaf79f8588
jnorindr <jade.norindr@gmail.com>
2023-11-15 17:34:51 +0100

    [TRAINING] correct import training

--- app/routes/training.py
+++ app/routes/training.py
@@ -1,7 +1,7 @@
 import os
 import zipfile
 from flask import request, jsonify
-# from os.path import exists
+from os.path import exists
 
 from app.app import app
 from app.utils.security import key_required
@@ -21,10 +21,10 @@ def send_dataset():
     anno_dir = DATASETS_PATH / dataset_name / "labels" / action
     yaml_file_path = DATA_PATH / f"{dataset_name}.yaml"
 
-    if not os.path.exists(imgs_dir):
+    if not exists(imgs_dir):
         os.makedirs(imgs_dir)
 
-    if not os.path.exists(anno_dir):
+    if not exists(anno_dir):
         os.makedirs(anno_dir)
 
     with zipfile.ZipFile(img_zip, 'r') as imgs:
@@ -37,3 +37,32 @@ def send_dataset():
         yaml.write(yaml_file.read().decode('utf-8'))
 
     return f"{dataset_name} sent for {action}"
+
+# TODO
+# @app.route('/test-model', methods=['POST'])
+# @key_required
+# def test_model():
+    # Lancer script test en asynchrone
+    # Param pour décider du modèle à tester
+    # Retourner résultats ?
+
+
+# @app.route('/train-model', methods=['POST'])
+# @key_required
+# def train_model():
+    # Lancer script entrainement
+    # Param pour décider du modèle + du dataset
+
+
+# TODO: create endpoint to get all available datasets
+# @app.route('/datasets', methods=['GET'])
+# def get_datasets():
+   # datasets_info = {}
+
+    # for filename in os.listdir(MODEL_PATH):
+    #     if filename.endswith(".pt"):
+    #        full_path = os.path.join(MODEL_PATH, filename)
+    #        modification_date = os.path.getmtime(full_path)
+    #        models_info[filename] = modification_date
+
+    # return jsonify(datasets_info)
